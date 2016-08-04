324fedfa4bd0b2227dca76b5a5da1c7332873436
Scott Humphries <git@sscotth.io>
2016-08-04 13:47:15 -0500

    add more tests

---
+++ src/math.js
@@ -0,0 +1,21 @@
+// Steps:
+
+// var add = 123
+// function add () {}
+// function add () { return 7 }
+// Added another test...
+
+function add (a, b) {
+  return parseFloat(a) + parseFloat(b);
+}
+
+// Steps:
+
+// var subtract = 123
+// function subtract () {}
+// function subtract () { return 2 }
+// Added another test...
+
+function subtract (a, b) {
+  return a - b
+}
--- src/math.test.js
+++ src/math.test.js
@@ -8,12 +8,28 @@ describe("The specification for basic math", function () {
     expect(add(2, 5)).toBe(7);
   });
 
+  it("should add two negative integers", function () {
+    expect(add(-2, -5)).toBe(-7);
+  });
+
+  it("should add decimal numbers", function () {
+    expect(add(.1, .2)).toBeCloseTo(.3);
+  });
+
+  it("should add two stringed integers", function () {
+    expect(add('2', '5')).toBe(7);
+  });
+
   it("should have a subtract function", function () {
     expect(subtract).toBeDefined();
   });
 
   it("should subtract two integers", function () {
     expect(subtract(7, 5)).toBe(2);
+  });
+
+  it("should subtract two negative integers", function () {
+    expect(subtract(-7, -5)).toBe(-2);
   });
 
 });
